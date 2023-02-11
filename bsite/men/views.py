88e6483d5cec0e61b042dde6b645f26aea44a97d
S-P-ring <alexbizonua@gmail.com>
2023-02-11 19:28:36 +0200

    add forms.py

--- bsite/men/views.py
+++ bsite/men/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
+
+from .forms import AddPostForm
 from .models import Men, Category
+
 
 def index(request):
     posts = Men.objects.all()
@@ -11,32 +14,55 @@ def index(request):
     }
     return render(request, 'men/index.html', context=context)
 
+
 def about(request):
-    return render(request, 'men/about.html',  {'menu': menu, 'title': 'О сайте'})
+    return render(request, 'men/about.html', {'menu': menu, 'title': 'О сайте'})
+
 
 def addpage(request):
-    return HttpResponse('Добавление статьи')
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+
+            try:
+                Men.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка при добавление нового поста')
+
+    else:
+        form = AddPostForm()
+    return render(request, 'men/addpage.html', {'title': 'Добавление статьи', 'form': form})
+
 
 def contact(request):
     return HttpResponse('Обратная связь')
 
+
 def login(request):
     return HttpResponse('Авторизация')
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отображение статьи с id = {post_id}')
 
-def show_category(request, cat_id):
-    posts = Men.objects.filter(cat_id=cat_id)
-    cat = Category.objects.filter(pk=cat_id)
+def show_post(request, post_slug):
+    post = get_object_or_404(Men, slug=post_slug)
+    context = {
+        'post': post,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+    return render(request, 'men/post.html', context=context)
+
+
+def show_category(request, cat_slug):
+    posts = Men.objects.filter(cat__slug=cat_slug)
+    cat = Category.objects.filter(slug=cat_slug)
     if len(posts) == 0:
         raise Http404()
 
     context = {
         'posts': posts,
-        'menu': menu,
         'title': f'Отображение по рубрике: "{cat[0].name}"',
-        'cat_selected': cat_id,
+        'cat_selected': cat_slug,
     }
 
     return render(request, 'men/index.html', context=context)
